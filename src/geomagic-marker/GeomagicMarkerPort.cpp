9435bebab260cab555526fa09dba1fc5f672fbb3
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2015-11-09 12:01:15 +0100

    geomagic-marker: Use geomagic frame instead of torso for the marker

--- src/geomagic-marker/GeomagicMarkerPort.cpp
+++ src/geomagic-marker/GeomagicMarkerPort.cpp
@@ -34,45 +34,12 @@ void GeomagicMarkerPort::onRead(yarp::os::Bottle &b)
     double qz = (cos(roll/2) * cos(pitch/2) * sin(yaw/2) - sin(roll/2) * sin(pitch/2) * cos(yaw/2));
     double qw = (cos(roll/2) * cos(pitch/2) * cos(yaw/2) + sin(roll/2) * sin(pitch/2) * sin(yaw/2));
 
-    visualization_msgs_Marker marker;
-    marker.header.frame_id = "/torso_chest";
-
     yarp::os::Stamp stamp;
     getEnvelope(stamp);
     if(!stamp.isValid()) {
         stamp.update();
     }
-    marker.header.stamp.sec = (int)stamp.getTime();
-    marker.header.stamp.nsec = 0; //FIXME
 
-    marker.lifetime.sec = 14; // FIXME
-    marker.lifetime.nsec = 0; //FIXME
-
-    marker.ns = "geomagic";
-
-    marker.type = visualization_msgs_Marker::SPHERE;
-    marker.action = visualization_msgs_Marker::ADD;
-    marker.id = 0;
-
-    marker.pose.position.x = x;
-    marker.pose.position.y = y;
-    marker.pose.position.z = z;
-
-    marker.pose.orientation.x = qx;
-    marker.pose.orientation.y = qy;
-    marker.pose.orientation.z = qz;
-    marker.pose.orientation.w = qw;
-
-    marker.scale.x = 0.15;
-    marker.scale.y = 0.10;
-    marker.scale.z = 0.05;
-
-    marker.color.r = 0.5f;
-    marker.color.g = 0.5f;
-    marker.color.b = 0.5f;
-    marker.color.a = 0.7f;
-
-    portOut->write(marker);
 
     tf2_msgs_TFMessage tf;
     geometry_msgs_TransformStamped ts;
@@ -91,4 +58,39 @@ void GeomagicMarkerPort::onRead(yarp::os::Bottle &b)
 
     tf.transforms.push_back(ts);
     portOutTF->write(tf);
+
+    visualization_msgs_Marker marker;
+    marker.header.frame_id = "/geomagic";
+
+    marker.header.stamp.sec = (int)stamp.getTime();
+    marker.header.stamp.nsec = 0; //FIXME
+
+    marker.lifetime.sec = 60;
+    marker.lifetime.nsec = 0;
+
+    marker.ns = "geomagic";
+
+    marker.type = visualization_msgs_Marker::SPHERE;
+    marker.action = visualization_msgs_Marker::ADD;
+    marker.id = 0;
+
+    marker.pose.position.x = 0;
+    marker.pose.position.y = 0;
+    marker.pose.position.z = 0;
+
+    marker.pose.orientation.x = 0.0;
+    marker.pose.orientation.y = 0.0;
+    marker.pose.orientation.z = 0.0;
+    marker.pose.orientation.w = 1.0;
+
+    marker.scale.x = 0.1;
+    marker.scale.y = 0.1;
+    marker.scale.z = 0.1;
+
+    marker.color.r = 1.0f;
+    marker.color.g = 0.5f;
+    marker.color.b = 0.5f;
+    marker.color.a = 0.7f;
+
+    portOut->write(marker);
 }
